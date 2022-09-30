50fb67c3dd7af1abd66dd3e9abdd6863e2584160
ridiculousigit <jsigit243@gmail.com>
2022-09-30 18:20:09 +0700

    Added move button at activity_main.xml

--- app/src/main/java/binar/academy/chapter5topic2/MainActivity.kt
+++ app/src/main/java/binar/academy/chapter5topic2/MainActivity.kt
@@ -3,43 +3,43 @@
 package binar.academy.chapter5topic2
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkConnectionType()
+        connectionType()
+
+        btnMove.setOnClickListener {
+            val move = Intent(this, DangerousActivity :: class.java)
+            startActivity(move)
+        }
     }
 
-    private fun checkConnectionType()
+    private fun connectionType()
     {
         val connectionManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val wifiConnection = connectionManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
-        val mobileConnection = connectionManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+        val koneksiWiFi = connectionManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+        val koneksidataSeluler = connectionManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
 
-        if (wifiConnection?.isConnectedOrConnecting == true)
-        {
-            toast("WIFI Connection is on")
-        }
-        else
-        {
-            if (mobileConnection?.isConnectedOrConnecting == true)
-            {
-                toast("Mobile Data Connection is on")
+        if (koneksiWiFi?.isConnectedOrConnecting != true) {
+            if (koneksidataSeluler?.isConnectedOrConnecting == true) {
+                toast("Koneksi Data Seluler Menyala !")
+            } else {
+                toast("Tak Ada Koneksi Internet !")
             }
-            else
-            {
-                toast("No Network Connection")
-            }
+        } else {
+            toast("Koneksi WiFi Menyala !")
         }
     }
 
-    fun toast(msg: String)
-    {
+    fun toast(msg: String) {
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
     }
 }
